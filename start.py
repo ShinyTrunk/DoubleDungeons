b88f1d37ae1b216dcb80dfa1d2ae359c3e22b28c
Fedor <fedorlegend@mail.ru>
2024-01-02 23:58:48 +0300

    first commit

---
+++ start.py
@@ -0,0 +1,177 @@
+import pygame
+import sys
+import os
+
+pygame.init()
+
+YELLOW = pygame.Color("yellow")
+RED = pygame.Color("red")
+GREEN = pygame.Color("#00FF00")
+BLUE = pygame.Color(0, 0, 255)
+BLACK = pygame.Color("#000000")
+WHITE = pygame.Color(255, 255, 255)
+SIZE = WIDTH, HEIGHT = (800, 550)
+
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+FPS = 50
+enemy_list = []
+player = None
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+box_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def start_screen():
+    intro_text = []
+
+    fon = pygame.transform.scale(load_image('starttt_screen.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    while True:
+        for event in pygame.event.get():
+            pos = pygame.mouse.get_pos()
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEBUTTONDOWN and 287 < pos[0] < 498 and 402 < pos[1] < 462:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+pygame.display.set_caption('Double Dungeons')
+tile_images = {
+    'wall': load_image('wood.png'),
+    'empty': load_image('stone.png')
+}
+player_image = load_image('knight.png')
+enemy1_image = load_image('broomstickman.png')
+
+tile_width = tile_height = 50
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(enemy_group, all_sprites)
+        self.image = enemy1_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 5, tile_height * pos_y + 5)
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites, box_group)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 5, tile_height * pos_y + 5)
+
+    def update(self, *args, **kwargs):
+        if args:
+            if args[0] == pygame.K_UP:
+                self.rect = self.rect.move(0, -50)
+            if args[0] == pygame.K_DOWN:
+                self.rect = self.rect.move(0, 50)
+            if args[0] == pygame.K_LEFT:
+                self.rect = self.rect.move(-50, 0)
+            if args[0] == pygame.K_RIGHT:
+                self.rect = self.rect.move(50, 0)
+        if pygame.sprite.spritecollide(self, box_group, False):
+            if args[0] == pygame.K_UP:
+                self.rect = self.rect.move(0, 50)
+            if args[0] == pygame.K_DOWN:
+                self.rect = self.rect.move(0, -50)
+            if args[0] == pygame.K_LEFT:
+                self.rect = self.rect.move(50, 0)
+            if args[0] == pygame.K_RIGHT:
+                self.rect = self.rect.move(-50, 0)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    px, py = None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Box('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                px, py = x, y
+            elif level[y][x] == '!':
+                Tile('empty', x, y)
+                enemy_list.append([x, y])
+    for i in enemy_list:
+        Enemy(i[0], i[1])
+    new_player = Player(px, py)
+    return new_player, x, y
+
+
+
+start_screen()
+player, level_x, level_y = generate_level(load_level('map.txt'))
+running = True
+while running:
+    clock.tick(60)
+    screen.fill(BLACK)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            player.update(event.key)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+terminate()
